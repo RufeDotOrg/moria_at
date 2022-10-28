fd56ac087d236ec90243389f984e9c84793bf788
Alan Newton <rufeooo@gmail.com>
2022-10-28 07:49:27 -0700

    cc.sh: build setup

---
+++ cc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+[ -d bin/ ] || mkdir bin
+[ -z "$CC" ] && CC=clang
+
+SYMLEVEL=1
+[ -n "$SYM" ] && SYMLEVEL=$SYM
+OPTLEVEL=1
+[ -n "$OPT" ] && OPTLEVEL=$OPT
+if [ -n "$ASAN" ]; then
+  OPTLEVEL=0
+  SYMLEVEL=3
+  DEV_FLAGS+=" -fsanitize=address "
+fi
+
+hash $CC || exit 1
+[ -z "$CFLAGS" ] && CFLAGS="-O${OPTLEVEL} -g${SYMLEVEL} -Isrc -fno-omit-frame-pointer -ferror-limit=3 "
+
+EXT=.${1##*.}
+OUTFILE=`basename $1 $EXT`
+
+case $EXT in
+  .cc ) ;&
+  .cpp )
+    CFLAGS+=" -fno-asynchronous-unwind-tables -fno-rtti -fno-exceptions -std=c++17 "
+    CFLAGS+=" -ffp-contract=off -freciprocal-math -fno-math-errno "
+
+    LFLAGS+=" -lstdc++ "
+    ;;
+esac
+CFLAGS+=" $DEV_FLAGS "
+
+TDIR=`dirname $1`
+[ -f $TDIR/ccext ] && source $TDIR/ccext
+
+echo $CC $1 $CFLAGS $LFLAGS -o bin/$OUTFILE
+time $CC $1 $CFLAGS $LFLAGS -o bin/$OUTFILE || exit 1
+
+if [ -n "$OD" ]; then
+  objdump -C -Mintel -d bin/$OUTFILE > fullobj_$OUTFILE
+fi
+
+if [ $# -gt 1 ]; then
+  shift
+  bin/$OUTFILE $@
+fi
