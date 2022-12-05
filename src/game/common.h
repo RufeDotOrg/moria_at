005a515c91a05578926b4cef7b8347cd490ab8ab
Alan Newton <rufeooo@gmail.com>
2022-12-05 10:13:51 -0800

    remove stdint.h

--- src/game/common.h
+++ src/game/common.h
@@ -1,5 +1,33 @@
-#include <stdint.h>
+// stdint
+typedef __UINT8_TYPE__ uint8_t;
+typedef __UINT16_TYPE__ uint16_t;
+typedef __UINT32_TYPE__ uint32_t;
+typedef __UINT64_TYPE__ uint64_t;
 
+typedef __INT8_TYPE__ int8_t;
+typedef __INT16_TYPE__ int16_t;
+typedef __INT32_TYPE__ int32_t;
+typedef __INT64_TYPE__ int64_t;
+
+#ifndef UINT64_MAX
+#define UINT64_MAX __UINT64_MAX__
+#define UINT32_MAX __UINT32_MAX__
+#define UINT16_MAX __UINT16_MAX__
+#define UINT8_MAX __UINT8_MAX__
+#define INT64_MAX __INT64_MAX__
+#define INT32_MAX __INT32_MAX__
+#define INT16_MAX __INT16_MAX__
+#define INT8_MAX __INT8_MAX__
+#endif
+
+#ifndef INT64_MIN
+#define INT64_MIN ((int64_t)~0x7fffffffffffffff)
+#define INT32_MIN ((int32_t)~0x7fffffff)
+#define INT16_MIN ((int16_t)~0x7fff)
+#define INT8_MIN ((int8_t)~0x7f)
+#endif
+
+// stdbool
 #define BOOL int
 
 // Default Type init
