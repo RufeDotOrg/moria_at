96aac0b6d70d488e7da178333eaf8ecc4b308559
Alan Newton <rufeooo@gmail.com>
2023-01-02 14:27:57 -0800

    version.sh: always display version; optional exe change

--- version.sh
+++ version.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-[ $# -eq 1 ] || exit 1
 
 MAJOR=`cat major | wc -l`
 MINOR=`cat minor | wc -l`
 COUNT=`cat release | wc -l`
 VER=`printf "%04d.%04d.%04d" $((MAJOR&0x3ff)) $((MINOR&0x3ff)) $((COUNT&0x3ff))`
-sed -i "s/\<XXXX.YYYY.ZZZZ\>/$VER/g" $1 || exit 2
 HASH=`git log -1 --format=%H | cut -c 1-15`
-sed -i "s/\<AbCdEfGhIjKlMnO\>/$HASH/g" $1 || exit 3
 echo "Version $VER Git Hash $HASH"
+
+[ $# -eq 1 ] || exit 1
+sed -i "s/\<XXXX.YYYY.ZZZZ\>/$VER/g" $1 || exit 2
+sed -i "s/\<AbCdEfGhIjKlMnO\>/$HASH/g" $1 || exit 3
+echo Modified executable $1
