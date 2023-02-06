fd3d0dd5cb1bf112b37a8ad57d501d6d41cb0453
Alan Newton <rufeooo@gmail.com>
2023-02-06 13:53:16 -0800

    code clean-up on random.c

--- platform/linux/random.c
+++ platform/linux/random.c
@@ -7,8 +7,8 @@ platform_random()
   int i, f, r;
   int val;
   f = GRND_RANDOM | GRND_NONBLOCK;
-  for (int i = 0; i < sizeof(val); i += r) {
-    r = getrandom(&val, sizeof(val) - i, f);
+  for (i = 0; i < sizeof(val); i += r) {
+    r = getrandom((char*)&val + i, sizeof(val) - i, f);
     if (r == -1 && errno == EINTR)
       r = 0;
     else if (r == -1 && errno == EAGAIN)
