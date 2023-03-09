471892ac1fe10d31fa92538acfc0f4096e29384a
Alan Newton <rufeooo@gmail.com>
2023-03-09 10:07:37 -0800

    bugfix for version.sh argument checks

--- version.sh
+++ version.sh
@@ -7,8 +7,6 @@ VER=`printf "%04d.%04d.%04d" $((MAJOR&0x3ff)) $((MINOR&0x3ff)) $((COUNT&0x3ff))`
 HASH=`git log -1 --format=%H | cut -c 1-15`
 echo "Version $VER Git Hash $HASH"
 
-[ $# -eq 1 ] || exit 1
-
 while getopts "a" opt; do
   case ${opt} in
     a )
@@ -22,6 +20,8 @@ while getopts "a" opt; do
   esac
 done
 shift $(($OPTIND - 1))
+
+[ $# -eq 1 ] || exit 1
 
 sed -i "s/\<XXXX.YYYY.ZZZZ\>/$VER/g" $1 || exit 2
 sed -i "s/\<AbCdEfGhIjKlMnO\>/$HASH/g" $1 || exit 3
