d492925e67c32519ceadb881a6b269f40d8134bb
Alan Newton <rufeooo@gmail.com>
2023-04-10 08:20:58 -0700

    example of store itemization

---
+++ example/store.c
@@ -0,0 +1,26 @@
+
+#define main _main
+#include "src/animal_run.c"
+#undef main
+
+int
+usage()
+{
+  puts("store <index>");
+  return 1;
+}
+
+int
+main(int argc, char** argv)
+{
+  if (argc != 2) return usage();
+
+  uint8_t sidx = strtoull(argv[1], 0, 10);
+  if (sidx < AL(store_choiceD)) {
+    for (int it = 0; it < AL(store_choiceD[0]); ++it) {
+      int tidx = store_choiceD[sidx][it];
+      printf("(%03d) %s\n", tidx, treasureD[tidx].name);
+    }
+  }
+  return 0;
+}
