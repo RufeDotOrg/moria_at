0894b0b49d6dffcf5de73e8a64bc784a0d89c81d
Alan Newton <rufeooo@gmail.com>
2023-04-17 10:42:28 -0700

    missing file.

---
+++ src/game/xxinline.c
@@ -0,0 +1,30 @@
+static int
+dir_x(dir)
+{
+  switch (dir) {
+    case 1:
+    case 4:
+    case 7:
+      return -1;
+    case 3:
+    case 6:
+    case 9:
+      return 1;
+  }
+  return 0;
+}
+static int
+dir_y(dir)
+{
+  switch (dir) {
+    case 1:
+    case 2:
+    case 3:
+      return 1;
+    case 7:
+    case 8:
+    case 9:
+      return -1;
+  }
+  return 0;
+}
