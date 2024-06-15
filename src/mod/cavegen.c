9aec5f5281245d4bd63c21236633e6e72a3779b0
Alan Newton <rufeooo@gmail.com>
2024-06-14 18:23:02 -0700

    cavegen.c: png output

--- src/mod/cavegen.c
+++ src/mod/cavegen.c
@@ -1,5 +1,8 @@
 
-static int checkD[MAX_HEIGHT][MAX_WIDTH];
+DATA int cave_pngD = 0;
+DATA int checkD[MAX_HEIGHT][MAX_WIDTH];
+DATA int imageD[MAX_HEIGHT][MAX_WIDTH];
+DATA int image_usedD;
 static void
 dfs(y, x)
 {
@@ -15,6 +18,57 @@ dfs(y, x)
   }
 }
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "third_party/stb/stb_image_write.h"
+static int
+image_write()
+{
+  char name[64];
+  int len = snprintf(AP(name), "cave_image%03d.png", image_usedD++);
+  int r = stbi_write_png(name, MAX_WIDTH, MAX_HEIGHT, 4, imageD,
+                         MAX_WIDTH * sizeof(int));
+  if (!r) printf("image_write failed\n");
+}
+static int
+cave_image()
+{
+  for (int row = 0; row < MAX_HEIGHT; ++row) {
+    for (int col = 0; col < MAX_WIDTH; ++col) {
+      struct caveS* c_ptr = &caveD[row][col];
+      int color = 0;
+      if (!c_ptr->fval) {
+        color = 0;
+      } else if (c_ptr->fval <= MAX_FLOOR) {
+        color = 50;
+      } else {
+        switch (c_ptr->fval) {
+          case BOUNDARY_WALL:
+            color = 100;
+            break;
+          case GRANITE_WALL:
+            color = 90;
+            break;
+          case MAGMA_WALL:
+            color = 80;
+            break;
+          case QUARTZ_WALL:
+            color = 70;
+            break;
+        }
+      }
+      imageD[row][col] = rgb_by_labr(color);
+    }
+  }
+}
+static int
+cave_debug()
+{
+  if (cave_pngD) {
+    cave_image();
+    image_write();
+  }
+}
+
 static int
 cave_check(y, x)
 {
@@ -23,7 +77,7 @@ cave_check(y, x)
   dfs(y, x);
   int count = 0;
   int fail = 0;
-  point_t first;
+  point_t first = {0, 0};
   for (int row = 0; row < MAX_HEIGHT; ++row) {
     for (int col = 0; col < MAX_WIDTH; ++col) {
       count += caveD[row][col].fval <= MAX_FLOOR;
@@ -40,40 +94,66 @@ cave_check(y, x)
          V2i(&first));
   return fail == 0;
 }
-// 0 OK; quit
-// 1 fail; load game
+
+enum { LOAD_GAME = 1 };
+enum { DLEV_BEGIN = 1 };
+enum { DLEV_END = 50 };
+enum { SEED_BEGIN = 0 };  // 5:14761
+enum { SEED_RANGE = 10 };
 int
 test_cavegen()
 {
-  int dlev = 1;
-  for (; dlev <= 50; ++dlev) {
-    int seed_count = 64 * 1024;
+  int ret = 0;
+  int dlev = DLEV_BEGIN;
+  for (; dlev < DLEV_END; ++dlev) {
     int pass = 0;
-    for (uint32_t it = 0; it < seed_count; ++it) {
+
+    int begin = SEED_BEGIN;
+    int end = SEED_BEGIN + SEED_RANGE;
+    for (uint32_t seed = begin; seed < end; ++seed) {
       hard_reset();
       dun_level = dlev;
-      rnd_seed = it;
+      rnd_seed = seed;
       cave_gen();
       pass = cave_check(uD.y, uD.x);
       if (!pass) {
+        {
+          struct caveS* c_ptr = &caveD[5][96];
+          printf("%d fval\n", c_ptr->fval);
+        }
+        if (!LOAD_GAME) {
+          hard_reset();
+          dun_level = dlev;
+          rnd_seed = seed;
+          cave_pngD = 1;
+          cave_gen();
+          printf("PNG output: dlev %d rnd_seed %d\n", dlev, seed);
+          struct caveS* c_ptr = &caveD[5][96];
+          printf("%d fval\n", c_ptr->fval);
+
+          exit(1);
+        }
+
         // Proceed to play for debug
         hard_reset();
         platformD.load(globalD.saveslot_class, 0);
         dun_level = dlev;
-        rnd_seed = it;
+        rnd_seed = seed;
         input_resumeD = -1;
-        dlev = 99;
-        return 1;
+        dlev = DLEV_END + 1;
+        ret = 1;
+        break;
       }
     }
-    printf("test result (%s): dlev %d | seed %d | seed_count %d\n", pass ? "ok" : "fail",
-           dun_level, rnd_seed, seed_count);
+    printf("test result (%s): dlev %d | SEED_RANGE %d\n", pass ? "ok" : "fail",
+           dun_level, SEED_RANGE);
   }
 
-  if (dlev < 99) printf("ALL dlev OK\n");
+  if (dlev <= DLEV_END) printf("ALL dlev OK\n");
+  printf("%d dlev\n", dlev);
   memcpy(death_descD, AP(quit_stringD));
 
-  return 0;
+  return ret && LOAD_GAME;
 }
 
 #define TEST_CAVEGEN 1
