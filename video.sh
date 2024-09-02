e9bc553407ed01d174a8cf8c9e0fa413084187c7
Alan Newton <rufeooo@gmail.com>
2024-09-02 06:28:42 -0700

    rm unused utility scripts

--- video.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NAME=cave
-ffmpeg -y -framerate 1 -i cave_image%03d.png -vf 'scale=iw*5:ih*5,pad=720:405:(((ow - iw)/2 )):(( (oh - ih)/2 ))' $NAME.mp4
-echo $? : $NAME.mp4
-ffmpeg -y -i $NAME.mp4 $NAME.gif 
-echo $? : $NAME.gif
